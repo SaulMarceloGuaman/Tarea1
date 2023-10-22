4ab102553642e2237fd7cf1aa3f399a4414b3c32
Saul Marcelo Guaman Cabrera <saulguamanc@gmail.com>
2023-10-22 11:34:38 -0500

    Update Project

--- UNIDAD_02/Tarea-API_RestFul/components/empresa/model.js
+++ UNIDAD_02/Tarea-API_RestFul/components/empresa/model.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+const req_string = {
+    type: String,
+    required: true
+}
+
 const empresa_schema = new Schema({
-    ruc: String,
-    nombre: String,
+    ruc: req_string,
+    nombre: req_string,
     domicilio: String,
     telefono: String
 })
